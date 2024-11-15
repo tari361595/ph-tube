9743ccc8dc9133b3eedd6f921951caaf19cc7a97
tari361595 <cmtariqulislam1994@gmail.com>
2024-11-15 22:39:31 +0900

    video titel and name done

--- script/video.js
+++ script/video.js
@@ -45,24 +45,24 @@ const loadVideos = () => {
 loadVideos();
 
 
-const cardDemo= {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
-    },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-}
+// const cardDemo= {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
 
 const displayVideos = (videos) => {
     // console.log(videos);
@@ -73,17 +73,26 @@ const displayVideos = (videos) => {
         const card = document.createElement('div')
     card.classList = "card card-compact";
     card.innerHTML = ` 
-    <figure>
+    <figure class = "h-[200px]">
     <img
       src=${video.thumbnail}
-      alt="Shoes" />
+      alt="Shoes" class = "w-full h-full object-cover" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="py-2 px-0 flex gap-2">
+  <div>
+  <img class = "w-10 h-10 rounded-full object-cover" src = "${video.authors[0].profile_picture}"/>
+  </div>
+  <div>
+  <h2 class = "font-bold">${video.title}</h2>
+  <div class = "flex items-center gap-2">
+   <p>${video.authors[0].profile_name}</p>
+   <img class="w-5" src = "https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
+  
+  </div>
+  <p></p>
+ 
+  </div>
+   
   </div>
 
 
