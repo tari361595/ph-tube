17d33d261c4dba5d3c9974ba7680dd6d5cf134ec
tari361595 <cmtariqulislam1994@gmail.com>
2024-11-20 21:52:24 +0900

    video category part by part done

--- script/video.js
+++ script/video.js
@@ -1,12 +1,12 @@
 // fetch,load and show catagories on html
 // crat loadCatagories
 // creat displayCatagories.
-function getTimeString(time){
-    const hour = parseInt(time/3600);
-let remaingSecond= time % 3600;
-const minute= parseInt(remaingSecond/60);
-remaingSecond=remaingSecond%60;
-return`${hour}hour${minute}minute ${remaingSecond}ago`;
+function getTimeString(time) {
+    const hour = parseInt(time / 3600);
+    let remaingSecond = time % 3600;
+    const minute = parseInt(remaingSecond / 60);
+    remaingSecond = remaingSecond % 60;
+    return `${hour}hour${minute}minute ${remaingSecond}ago`;
 }
 
 
@@ -16,6 +16,14 @@ const loadCategories = () => {
         .then((data) => displayCategories(data.categories))
         .catch((error) => console.error("Error loading categories:", error));
 };
+
+const loadCategoryVideos = (id) => {
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.category))
+        .catch((error) => console.error("Error loading categories:", error));
+}
 
 const displayCategories = (categories) => {
     const categoriesContainer = document.getElementById('categories');
@@ -30,10 +38,13 @@ const displayCategories = (categories) => {
     categories.forEach((item) => {
         console.log(item);
         // Create a button
-        const button = document.createElement('button');
-        button.classList.add('btn');
-        button.innerText = item.category; // Fixed property name
-        categoriesContainer.appendChild(button); // Fixed appendChild method
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML =
+            `<button onclick="loadCategoryVideos(${item.category_id})" class ="btn">
+        ${item.category}
+        </button>
+        `
+        categoriesContainer.appendChild(buttonContainer); // Fixed appendChild method
     });
 };
 
@@ -73,6 +84,7 @@ loadVideos();
 const displayVideos = (videos) => {
     // console.log(videos);
     const videosContainer = document.getElementById('videos')
+    videosContainer.innerHTML = "";
     videos.forEach((video) => {
 
         console.log(video);
@@ -83,21 +95,24 @@ const displayVideos = (videos) => {
     <img
       src=${video.thumbnail}
       alt="Shoes" class = "w-full h-full object-cover" />
-      ${video.others.posted_date?.length === 0? " " 
-        :`<span class ="absolute right-2 bottom-2 bg-black rounded p-1 text-white">
+      ${video.others.posted_date?.length === 0 ? " "
+                : `<span class ="absolute text-xs right-2 bottom-2 bg-black rounded p-1
+         text-white">
             ${getTimeString(video.others.posted_date)}</span>`}
       
   </figure>
   <div class="py-2 px-0 flex gap-2">
   <div>
-  <img class = "w-10 h-10 rounded-full object-cover" src = "${video.authors[0].profile_picture}"/>
+   <img class = "w-10 h-10 rounded-full object-cover" 
+   src = "${video.authors[0].profile_picture}"/>
   </div>
   <div>
   <h2 class = "font-bold">${video.title}</h2>
   <div class = "flex items-center gap-2">
    <p>${video.authors[0].profile_name}</p>
-   ${video.authors[0].verified === true ? `<img class="w-5" src = "https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : " "
-   }
+   ${video.authors[0].verified === true ? `<img class="w-5" 
+    src = "https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : " "
+            }
    
   
   </div>
